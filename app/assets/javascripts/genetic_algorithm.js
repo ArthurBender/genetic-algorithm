34d70ed114fb86a9adc1d0644864d2b0db6e81b9
Arthur Bender <arthur@dba.eng.br>
2020-01-15 15:37:17 -0300

    Adicionado código de calculo de fitness

--- app/assets/javascripts/genetic_algorithm.js
+++ app/assets/javascripts/genetic_algorithm.js
@@ -7,6 +7,7 @@ function discover_word(word, population, mutation, last_result_el, generations_e
         alert("CARAIO QUE CAGADA IRMAO");
     } else {
         results_history_el.text(firstGenerationReturn.join("\n"));
+        console.log(JSON.stringify(calculateFitness(firstGenerationReturn, word)));
     }
 };
 
@@ -30,3 +31,31 @@ function firstGeneration(word, population, last_result_el) {
 
     return seedArray;
 };
+
+function calculateFitness(generationArray, word) {
+    var fitArray = [];
+    for(var i = 0; i < generationArray.length; i++) {
+        var fitScore = 0;
+        for(var l = 0; l < word.length; l++) {
+            if(generationArray[i][l] == word[l]) {
+                fitScore += 1;
+            }
+        }
+        fitArray.push(fitScore);
+    }
+
+    var scorePercentagePiece = 100.0 / fitArray.reduce((a,b) => a + b, 0);
+    var totalPercentage = 0.0;
+    var wordsPercentage = [];
+
+    for(var i = 0; i < fitArray.length; i++) {
+        if(fitArray[i] != 0) {
+            totalPercentage += fitArray[i] * scorePercentagePiece;
+            wordsPercentage.push({percentage: totalPercentage, word: generationArray[i]});
+        } else {
+            wordsPercentage.push({percentage: 0, word: generationArray[i]});
+        }
+    }
+
+    return wordsPercentage;
+}
